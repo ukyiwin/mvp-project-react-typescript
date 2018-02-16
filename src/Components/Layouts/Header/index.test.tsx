55750ff32cd4e92b23652425cf8fe268069dded9
Rex Raphael <rex.raphael@outlook.com>
2018-02-16 18:13:10 +0100

    Remove header test for now

--- src/Components/Layouts/Header/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Header from './index';
-
-it('header renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
-});
