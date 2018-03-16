1340221e25ba177277aba03123c249ba880d1af0
Rex Raphael <rex.raphael@outlook.com>
2018-03-16 20:17:10 +0100

    Fixed wrong url

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ const AUTH_TOKEN = 'token';
 
 // const queueLink = new QueueLink();
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+const httpLink = new HttpLink({ uri: 'http://uniserver.now.sh' });
 
 const defaults = { appState: 'INITIAL' };
 const errorLink = onError(({ graphQLErrors, networkError }) => {
